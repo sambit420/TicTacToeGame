63af62352982a84fb66d1d1883c2d8092c697bd2
sambit420 <sambit2024@gmail.com>
2022-10-24 05:56:29 +0530

    Welcome to Tic Tac Toe Game !!!

---
+++ src/com/tictactoe/program/TicTacToe.java
@@ -0,0 +1,14 @@
+package com.tictactoe.program;
+
+public class TicTacToe {
+    //Creating a default constructor
+    public TicTacToe() {
+        System.out.println("Welcome to Tic Tac Toe Game !!!");
+    }
+
+    public static void main(String[] args) {
+
+        //Initialize the object
+        TicTacToe obj = new TicTacToe();
+}
+}
