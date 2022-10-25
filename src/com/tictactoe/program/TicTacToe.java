687e95dd6a90a4a3d7d04c952c6f78ce2eeb9a37
sambit420 <sambit2024@gmail.com>
2022-10-25 23:38:39 +0530

    Added : Use Case 2 Program - Allow player to choose option 'X' or 'O'

--- src/com/tictactoe/program/TicTacToe.java
+++ src/com/tictactoe/program/TicTacToe.java
@@ -1,6 +1,9 @@
 //This program is all about Tic Tac Toe Game.
 //Starting is to show a Welcome message to this game.
 //Use Case 1 is to create the Tic Tac Toe board.
+//Use Case 2 is to allow player to choose letter X or O
+
+
 package com.tictactoe.program;
 
 import java.util.Scanner;
@@ -10,16 +13,9 @@ public class TicTacToe {
     public TicTacToe() {
         System.out.println("Welcome to Tic Tac Toe Game !!!");
     }
-
     static char[] board = new char[10];
     static char player, computer;
-    Scanner scan = new Scanner(System.in);
-
-    public void initialize() {
-        for (int i = 1; i < 10; i++) {
-            board[i] = ' ';
-        }
-    }
+    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -32,6 +28,36 @@ public class TicTacToe {
         System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("---------");
         System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+
+        //Checking for player choice
+        chooseOption();
+
+    }
+
+    //Initialization of game
+    public static void initialize() {
+        for (int i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+    }
+
+    //Allow player to choose X or O
+    public static void chooseOption()
+    {
+        System.out.println("Please Select Your Choice Letter : \nProvide 'X' or 'O'");
+        char choice = scan.next().charAt(0);
+        if (choice == 'X' || choice == 'x')
+        {
+            player = 'X';
+            computer = 'O';
+        }
+        else if (choice == 'O' || choice == 'o')
+        {
+            player = 'O';
+            computer = 'X';
+        }
+        System.out.println("Player choosing option : "+player);
+
 
     }
 }
